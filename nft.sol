1d9f22e0d0718c9c2f05160781a4b828c6646e0b
Mike Rokita <michalr@piazzaro.io>
2024-04-10 20:10:33 +0200

    feat: nft minting contract

---
+++ nft.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FunticoNFT is ERC721, ERC721Enumerable, Ownable {
+    uint256 private _nextTokenId;
+    uint256 public MAX_SUPPLY = 900;
+
+    constructor(
+        address initialOwner
+    ) ERC721("Funtico Summoners", "FSC") Ownable(initialOwner) {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://..../metadata01/";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        require(totalSupply() < MAX_SUPPLY, "Maximum supply reached");
+
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+
+    // enable owner to set new max supply
+    function updateMaxSupply(uint256 _max) public onlyOwner {
+        if (totalSupply() < _max) {
+            MAX_SUPPLY = _max;
+        }
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
